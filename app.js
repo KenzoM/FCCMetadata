23d5bd3df248f57dc2bbddfc99e03d5ac06a000c
Kenzo Mendoza <kenzom954@gmail.com>
2016-08-08 12:43:38 -0700

    integrated index.html with expres

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var multer = require('multer');
+var path = require('path')
 var port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/',function(req, res){
-  res.send('hello world')
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 })
 
 
