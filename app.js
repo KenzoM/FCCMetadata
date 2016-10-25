7e63bb341e651d0b0a0193e2de03b3ebc1765e6d
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-25 15:27:09 -0700

    replaced diskStorage to MemoryStorage

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 var express = require("express");
 var multer  = require('multer');
 var app = express();
-var storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './uploads');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now());
-  }
-});
+var storage = multer.memoryStorage();
 var upload = multer({ storage : storage}).single('fileExamine');
 var port = process.env.PORT || 8000
 
@@ -19,7 +12,6 @@ app.get('/',function(req,res){
 app.post('/api/filemeta',function(req,res){
   upload(req,res,function(err) {
       if(err) {
-        console.log(err,'this is error message!!!')
         return res.end("Error uploading file.");
       }
       res.json({size: req.file.size});
